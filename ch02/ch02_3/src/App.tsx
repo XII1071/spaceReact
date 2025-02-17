146401fb3b16cc508de41001bbfc683644c2e14d
XII1071 <garmen1071@gmail.com>
2025-02-17 15:40:58 +0900

    0217

--- ch02/ch02_3/src/App.tsx
+++ ch02/ch02_3/src/App.tsx
@@ -1,51 +1,64 @@
-import {Component, ReactNode, useState} from 'react'
+import {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import {Component} from 'react'
 import './App.css'
+import ClassComponent from './ClassComponent'
 
-// 사용자 컴포넌트 중 함수형 컴포넌트
-// export default function App() {
-
-// }
-
-// 사용자 컴포넌트 중 클래스스 컴포넌트
-export default class App2 extends Component {
+// 사용자 컴포넌트 중 클래스 컴포넌트
+export default class App extends Component {
   render() {
+    const isLoading = false
+    if (isLoading) return <p>Loading.......</p>
+
     return (
-      <ul>
-        <li>
-          <a href="https://www.meta.com">
-            <p>Meta</p>
-          </a>
-        </li>
-      </ul>
+      <>
+        <ul style={{background: 'yellow'}}>
+          {/* <ClassComponent />
+          <ClassComponent /> */}
+        </ul>
+        <ul style={{background: 'black', color: 'white'}}>
+          {!isLoading ? (
+            <p>Loading...</p>
+          ) : (
+            <ClassComponent href="https://www.naver.com" text="Naver" />
+          )}
+          {!isLoading && <ClassComponent href="https://www.naver.com" text="Naver" />}
+        </ul>
+      </>
     )
   }
 }
 
-function App() {
-  const [count, setCount] = useState(0)
+// 사용자 컴포넌트 중 함수형 컴포넌트 :: 합쳐 놓은 형태
+// export default function App() {
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
-  )
-}
+// }
 
-export default App
+// 사용자 컴포넌트 중 함수형 컴포넌트 :: 함수와 export를 분리한 형태
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+//     </>
+//   )
+// }
+
+// export default App
---
+++ ch02/ch02_3/src/ClassComponent.tsx
@@ -0,0 +1,32 @@
+import {Component} from 'react'
+
+// 사용자 컴포넌트 중 클래스 컴포넌트 :: 속성이 없는 경우
+/*export default class ClassComponent extends Component {
+  render() {
+    return (
+      <li>
+        <a href="https://www.meta.com">
+          <p>Meta</p>
+        </a>
+      </li>
+    )
+  }
+}*/
+
+// 사용자 컴포넌트 중 클래스 컴포넌트 :: 속성이 있는 경우
+export type ClassComponentProps = {
+  href: string
+  text: string
+}
+export default class ClassComponent extends Component<ClassComponentProps> {
+  render() {
+    const {href, text, ...rest} = this.props
+    return (
+      <li>
+        <a href={href}>
+          <p>{text}</p>
+        </a>
+      </li>
+    )
+  }
+}
