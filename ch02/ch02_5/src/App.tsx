eae00629f6ae59193be0dd3e15a024dcce2737cc
XII1071 <garmen1071@gmail.com>
2025-02-18 11:32:17 +0900

    0218

--- ch02/ch02_5/src/App.tsx
+++ ch02/ch02_5/src/App.tsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import DispatchEvent from './pages/DispatchEvent'
+import DragDrop from './pages/DragDrop'
+import EventBubbling from './pages/EventBubbling'
+import EventListener from './pages/EventListener'
+import FileDrop from './pages/FileDrop'
+import FileInput from './pages/FileInput'
+import OnChange from './pages/OnChange'
+import OnClick from './pages/OnClick'
+import ReactOnClick from './pages/ReactOnClick'
+import StopPropagation from './pages/StopPropagation'
+import VariousInputs from './pages/VariousInputs'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <FileDrop />
+      <DragDrop />
+      <FileInput />
+      <OnChange />
+      <VariousInputs />
+      <StopPropagation />
+      <EventBubbling />
+      {/* <DispatchEvent />
+      <ReactOnClick />
+      <OnClick />
+      <EventListener /> */}
+    </div>
   )
 }
 
