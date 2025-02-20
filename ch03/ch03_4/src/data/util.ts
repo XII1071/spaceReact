b9a32d20b5a04da5a3d1bd7a6b6b6575a16a4482
XII1071 <garmen1071@gmail.com>
2025-02-20 14:59:11 +0900

    0220

--- ch03/ch03_4/src/data/util.ts
+++ ch03/ch03_4/src/data/util.ts
@@ -1,9 +1,8 @@
-export const makeArray = function (length: number) {
-  return new Array(length).fill(null)
-}
-export const range = function (min: number, max: number): number[] {
-  return makeArray(max - min).map((notUsed, index) => index + min)
-}
-export const random = function (min: number, max: number): number {
-  return Math.floor(Math.random() * (max - min)) + min
-}
+//null로 fill한 이유: undefined를 null로 주면 배열의 map적용시 에러 안남.
+export const makeArray = (length: number) => new Array(length).fill(null)
+
+export const range = (min: number, max: number): number[] =>
+  makeArray(max - min).map((notUsed, index) => index + min)
+
+export const random = (min: number, max: number): number =>
+  Math.floor(Math.random() * (max - min)) + min
