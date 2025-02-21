92c8a901bf13161d01ecd805b4ae69ea5b42a353
XII1071 <garmen1071@gmail.com>
2025-02-21 14:19:24 +0900

    0221

---
+++ ch03/ch03_login/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main>
+      <h2 className="text-xl bg-amber-200">Test</h2>
+      <button className="btn btn-primary">daisy</button>
+    </main>
+  )
+}
