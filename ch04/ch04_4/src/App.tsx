a5831e974bde44ee6477b4ea49902edf077483c6
XII1071 <garmen1071@gmail.com>
2025-02-26 14:34:08 +0900

    0226

--- ch04/ch04_4/src/App.tsx
+++ ch04/ch04_4/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <main>
       <h1 className="bg-blue-500 text-white text-5xl text-center p-3">
-        생명주기와 관련이 있는 useEffect, useLayoutEffect 훅
+        생명주기와 관련이 있는 useEffect(비동기), useLayoutEffect(동기) 훅
       </h1>
       <FetchTest />
       <WindowResizeTest />
--- ch04/ch04_4/src/pages/WindowResizeTest.tsx
+++ ch04/ch04_4/src/pages/WindowResizeTest.tsx
@@ -2,7 +2,7 @@ import {Title, Subtitle} from '../components'
 import {useWindowResize} from '../hooks'
 
 export default function WindowResizeTest() {
-  const [width, height] = useWindowResize()
+  const [width, height] = useWindowResize() //커스텀 훅 호출
   return (
     <section className="mt-4">
       <Title>WindowResizeTest</Title>
