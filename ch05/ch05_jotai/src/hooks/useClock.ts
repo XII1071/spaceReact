5062c42d40776b877ced3b2ba4ae89e42280dbbb
XII1071 <garmen1071@gmail.com>
2025-02-28 16:43:29 +0900

    0228

--- ch05/ch05_jotai/src/hooks/useClock.ts
+++
@@ -1,8 +0,0 @@
-import {useState} from 'react'
-import {useInterval} from './useInterval'
-
-export const useClock = function () {
-  const [today, setToday] = useState(new Date())
-  useInterval(() => setToday(new Date()))
-  return today
-}
--- ch05/ch05_jotai/src/store/AppState.ts
+++
@@ -1,1 +0,0 @@
-export type AppState = {today: Date} // 사용할 변수 선언
