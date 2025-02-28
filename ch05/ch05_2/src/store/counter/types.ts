9f39d111ef72d1854bf0aa74da9acb79b90b1d78
XII1071 <garmen1071@gmail.com>
2025-02-28 11:39:49 +0900

    0228

--- ch05/ch05_2/src/store/counter/types.ts
+++ ch05/ch05_2/src/store/counter/types.ts
@@ -1,4 +1,6 @@
 import type {Action} from 'redux'
 
-export type State = any
-export type Actions = Action
+export type State = number
+export type SetCounterAction = Action<'@counter/setCounter'> & {payload: State}
+
+export type Actions = SetCounterAction
